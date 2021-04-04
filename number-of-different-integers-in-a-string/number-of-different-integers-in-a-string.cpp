93e150c81530337a3a7793b5fef9b2791ffd4017
Nikhil Rajput <55176863+nikhraj@users.noreply.github.com>
2021-04-04 15:25:34 +0530

    Time: 4 ms (61.13%), Space: 6.4 MB (90.66%) - LeetHub

--- number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.cpp
+++ number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int numDifferentIntegers(string s) {
-        //int ans=0;
-       // map <long long,int> m;
         set <string> ans;
         for(int i=0;i<s.size();i++)
         {
             if(isdigit(s[i]))
             {
                 string temp="";
-                //ans++;
                 while(isdigit(s[i]))
                 {
                     temp+=s[i];
@@ -19,7 +16,6 @@ public:
                 ans.insert(temp);
             }
         }
-        
         return ans.size();
     }
 };
