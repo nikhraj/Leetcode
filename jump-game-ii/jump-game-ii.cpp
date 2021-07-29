86d6076cc6a5ba38c8fb90feee934b834185e0f3
Nikhil Rajput <55176863+nikhraj@users.noreply.github.com>
2021-07-29 21:31:48 +0530

    Time: 16 ms (67.68%), Space: 16.4 MB (37.54%) - LeetHub

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector <int> dp(n,n+1);
-        dp[0]=0;
+        int ans=0;
+        int prev = 0;
+        int global = 0;
+        
         for(int i=0;i<n-1;i++)
         {
-            for(int j=i+1;j<n&&j<=(i+nums[i]);j++)
+            global=max(global,i+nums[i]);
+            if(i==prev)
             {
-                dp[j]=min(dp[j],dp[i]+1);
+                ans++;
+                prev=global;
             }
+            
         }
-        // for(int i:dp)
-        //     cout<<i<<endl;
-        return dp[n-1];
         
+        return ans;
     }
 };
