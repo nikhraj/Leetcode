3fccca6b09b9149c2e5f114aec4a1f249e9f5d75
Nikhil Rajput <55176863+nikhraj@users.noreply.github.com>
2021-07-30 22:07:23 +0530

    Time: 8 ms (89.18%), Space: 16.4 MB (15.28%) - LeetHub

--- minimum-window-substring/minimum-window-substring.cpp
+++ minimum-window-substring/minimum-window-substring.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector <int> m(58,0);
+        int m=s.size(),n=t.size();
+        int count = n;
+        int l=0,r=0;
+        vector <int> ma(58);
         for(char c:t)
         {
-            m[c-'A']++;
+            ma[c-'A']++;
         }
-        int count = t.size();
-        int n = s.size();
-        int l=0,r=0;
-        string ans = "";
-        int mini = n+1;
-        while(r<n)
+        string ans;
+        int c=INT_MAX;
+        while(r<m)
         {
-            if(--m[s[r++]-'A']>=0)
+            if(--ma[s[r++]-'A']>=0)
                 count--;
             while(count==0)
             {
-                if((r-l)<mini)
+                if(r-l<c)
                 {
-                    mini=r-l;
-                    ans = s.substr(l,r-l);
+                    c=r-l;
+                    ans=s.substr(l,r-l);
                 }
-                if(++m[s[l++]-'A']>0)
+                if(++ma[s[l++]-'A']>=1)
                     count++;
             }
         }
         
         return ans;
-        
-        
     }
 };
