d06f378623ae1d6e1c2ba1fd3c016e6a90c9c147
Nikhil Rajput <55176863+nikhraj@users.noreply.github.com>
2021-10-07 15:48:48 +0530

    Time: 632 ms (22.34%), Space: 7.4 MB (82.16%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -2,39 +2,39 @@ class Solution {
 public:
     int m,n;
     bool exist(vector<vector<char>>& board, string word) {
-        char c = word[0];
         m = board.size();
         n = board[0].size();
-        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(board[i][j]==c)
+                char c = board[i][j];
+                if(word[0]==c)
                 {
-                    if(dfs(i,j,word,board))
+                    if(dfs(i,j,board,word))
                         return true;
                 }
             }
         }
-        
         return false;
     }
     
-    bool dfs(int i,int j,string word,vector <vector <char>> &board)
+    bool dfs(int i,int j,vector <vector <char>> &grid,string word)
     {
-        if(i<0||j<0||i>=m||j>=n||board[i][j]!=word[0]||board[i][j]=='-1')
-            return false;
-        if(word.size()==1)
+        if(word.size()==0)
             return true;
+        if(i<0||j<0||i>=m||j>=n||grid[i][j]!=word[0])
+        {
+            return false;
+        }
+        char c = grid[i][j];
+        grid[i][j]='1';
         
-        char c=board[i][j];
-        board[i][j]='-1';
         word = word.substr(1);
-        bool f = dfs(i-1,j,word,board)||dfs(i+1,j,word,board)||dfs(i,j-1,word,board)||dfs(i,j+1,word,board);
-        board[i][j]=c;
-            
+        bool f = dfs(i-1,j,grid,word)|| dfs(i+1,j,grid,word)|| dfs(i,j-1,grid,word)|| dfs(i,j+1,grid,word);
+        grid[i][j] = c;
         return f;
         
     }
+    
 };
